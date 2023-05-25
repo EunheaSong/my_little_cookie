6397811acf00f33fd5f157d749889b9cf5f335d9
song-eunhye <dmsgp8292@gmail.com>
2023-05-25 19:42:46 +0900

    first commit

---
+++ src/main/kotlin/com/mlc/my_little_cookie/MyLittleCookieApplication.kt
@@ -0,0 +1,11 @@
+package com.mlc.my_little_cookie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyLittleCookieApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyLittleCookieApplication>(*args)
+}
---
+++ src/test/kotlin/com/mlc/my_little_cookie/MyLittleCookieApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mlc.my_little_cookie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyLittleCookieApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
